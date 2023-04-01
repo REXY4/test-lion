7570c1c235d4e8e5661a961ab12df13c798a1e3a
REXY4 <muhammadrzki96@gmail.com>
2023-04-02 02:56:06 +0700

    complete

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,15 +1,15 @@
 module.exports = {
     // this will check Typescript files
-    '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+    './src/**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
   
     // This will lint and format TypeScript and
     '**/*.(ts|tsx|js)': (filenames) => [
-      `yarn eslint --fix ${filenames.join(' ')}`,
-      `yarn prettier --write ${filenames.join(' ')}`,
+      `npm run eslint --fix ${filenames.join(' ')}`,
+      `npm run prettier --write ${filenames.join(' ')}`,
     ],
   
     // this will Format MarkDown and JSON
     '**/*.(md|json)': (filenames) =>
-      `yarn prettier --write ${filenames.join(' ')}`,
+      `npm run prettier --write ${filenames.join(' ')}`,
   }
   
